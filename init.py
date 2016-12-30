29fcc2014535a8f7646529778ff4d3a5b58489fd
m85091081 <m85091081@gmail.com>
2016-12-31 00:06:36 +0800

    clean code fix mu_sanic runtime error

--- init.py
+++ init.py
@@ -25,10 +25,19 @@ try:
 except OSError as e:
     raise e
 
+def runtime_gen():
+    subprocess.call("echo 'from %s.core_view.index import main' >> %s/runtime.py " % (user_project,new_folder),shell=True)
+    subprocess.call("echo '\n' >> %s/runtime.py " % (new_folder),shell=True)
+    subprocess.call("echo '%s_runapp = Sanic(__name__)' >> %s/runtime.py " % (user_project,new_folder),shell=True)
+    subprocess.call("echo '%s_runapp.blueprint(main)' >> %s/runtime.py " % (user_project,new_folder),shell=True)
+    subprocess.call("echo '{0}_runapp.static(\"/static\",\"./\%s/core_static\" % config.your_app)' >> {1}/runtime.py ".format(user_project,new_folder),shell=True)
+    subprocess.call("echo '%s_runapp.run(host=\"0.0.0.0\", port=8000, debug=True)' >> %s/runtime.py " % (user_project,new_folder),shell=True)
+    
 old_app = new_folder+'/app'
 new_app = new_folder+'/%s' % user_project
 os.rename(old_app,new_app)
 subprocess.call("echo 'your_app=\"" + user_project + "\"' >> " + new_folder + '/mu_sanic/config.py',shell=True)
+runtime_gen()
 print('Your mu_sanic project create in %s' % new_folder )
 print('Your mu_sanic project app create in %s' % new_app )
 print('Your mu_sanic project config create in %s/mu_sanic/config.py' % new_folder )
